7b115c5b124fb755caef60303e78451331532e23
Elaine Hale <elaine.hale@nrel.gov>
2023-02-20 15:48:32 -0700

    Start at creating specific project for computing DPV projections (workaround for inability to have multiple metric dimensions per project).

---
+++ dsgrid_project/datasets/modeled/dpv_workaround_project/scripts/test-project.py
@@ -0,0 +1,19 @@
+"""
+Script to test the DPV workaround project
+
+--------------
+CLI COMMANDS:
+--------------
+# register the project
+dsgrid registry --offline projects register "dsgrid_project/datasets/modeled/dpv_workaround_project/project.json5" -l "test"
+
+# register the datasets
+
+# submit the datasets to project
+
+# create the generation shape (capacity factors) derived dataset
+
+# apply the generation shapes to the dGen projected capacities to create 
+# the projected dataset
+
+"""
